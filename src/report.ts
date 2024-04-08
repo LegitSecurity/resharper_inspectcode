360a165ebfb634af1db74ea8e7d2a58c8dbb5af6
Benjie-legit <benjie@legitsecurity.com>
2024-04-08 17:33:22 +0300

    test

--- src/report.ts
+++ src/report.ts
@@ -114,24 +114,29 @@ export class Report {
   }
 
   output(): void {
-    for (const issue of this.issues) {
-      const properties: {[key: string]: string | number} = {}
-
-      properties['file'] = issue.FilePath
-      if (issue.Line) {
-        properties['line'] = issue.Line
-        properties['col'] = issue.Column
-      }
-
-      issueCommand(issue.Severity, properties, issue.output())
+  for (const issue of this.issues) {
+    if (issue.Severity === 'notice') {
+      continue; // Skip notices
     }
+
+    const properties: {[key: string]: string | number} = {};
+
+    properties['file'] = issue.FilePath;
+    if (issue.Line) {
+      properties['line'] = issue.Line;
+      properties['col'] = issue.Column;
+    }
+
+    issueCommand(issue.Severity, properties, issue.output());
   }
+}
 
   issueOverThresholdIsExists(minimumSeverity: string): boolean {
-    const errorTarget = this.switchErrorTarget(minimumSeverity)
+    const errorTarget = this.switchErrorTarget(minimumSeverity);
 
-    return this.issues.filter(i => errorTarget.includes(i.Severity)).length > 0
+    return this.issues.filter(i => i.Severity !== 'notice' && errorTarget.includes(i.Severity)).length > 0;
   }
+
 
   private switchErrorTarget(minimumSeverity: string): Severity[] {
     if (minimumSeverity === 'error') {
